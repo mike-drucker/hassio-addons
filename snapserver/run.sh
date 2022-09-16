68aaa6068532c6379e419e70f0aa34d31d58908a
mike-drucker <mike-drucker@users.noreply.github.com>
2022-09-16 14:32:52 -0500

    debug info

--- snapserver/run.sh
+++ snapserver/run.sh
@@ -12,32 +12,34 @@ fi
 bashio::log.info "Populating snapserver.conf..."
 
 # Start creation of configuration
-
+bashio::log.info " 1 "
 echo "[stream]" > "${config}"
 for stream in $(bashio::config 'stream.streams'); do
     echo "stream = ${stream}" >> "${config}"
 done
+bashio::log.info " 2 "
 echo "buffer = $(bashio::config 'stream.buffer')" >> "${config}"
+bashio::log.info " 3 "
 echo "codec = $(bashio::config 'stream.codec')" >> "${config}"
 echo "send_to_muted = $(bashio::config 'stream.send_to_muted')" >> "${config}"
 echo "sampleformat = $(bashio::config 'stream.sampleformat')" >> "${config}"
-
+bashio::log.info " 4 "
 echo "[http]" >> "${config}"
 echo "enabled = $(bashio::config 'http.enabled')" >> "${config}"
 echo "doc_root = $(bashio::config 'http.docroot')" >> "${config}"
-
+bashio::log.info " 5 "
 echo "[tcp]" >> "${config}"
 echo "enabled = $(bashio::config 'tcp.enabled')" >> "${config}"
-
+bashio::log.info " 6 "
 echo "[logging]" >> "${config}"
 echo "debug = $(bashio::config 'logging.enabled')" >> "${config}"
-
+bashio::log.info " ##### "
 echo "[server]" >> "${config}"
 echo "threads = $(bashio::config 'server.threads')" >> "${config}"
-
+bashio::log.info " 7 "
 echo "[server]" >> "${config}"
 echo "datadir = $(bashio::config 'server.datadir')" >> "${config}"
-
+bashio::log.info " 8 "
 bashio::log.info "Starting SnapServer..."
-
+bashio::log.info << /etc/snapserver.conf
 /usr/bin/snapserver -c /etc/snapserver.conf
